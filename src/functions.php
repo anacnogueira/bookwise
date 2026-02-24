7ee5192bc5830c3097d3ba13259397985ae0bb98
Ana Claudia <anacnogueira@gmail.com>
2026-02-24 10:05:22 -0300

    Ultimas modificações

---
+++ src/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+function dd(...$dump)
+{
+    echo "<pre>";
+    var_dump($dump);
+    echo "</pre>";
+
+    die();
+}
+
+function abort($code)
+{
+    http_response_code($code);
+    view($code);
+    die();
+}
+
+function view($view, $data = [])
+{
+    foreach($data as $key => $value) {
+
+        $$key = $value;
+
+    }
+
+    require 'views/template/app.php';
+}
---
+++ src/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+function loadController()
+{
+    $uri = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
+
+    $controller = !empty($uri) ? $uri : 'index';
+
+    $path = "controllers/{$controller}.controller.php";
+
+    if (!file_exists($path)) {
+        abort(404);      
+    }
+    require "controllers/{$controller}.controller.php";
+}
+
+loadController();
