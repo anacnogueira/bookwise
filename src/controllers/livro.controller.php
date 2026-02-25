462f6eb5b35658626f42ca736a4373e2ac2a816b
Ana Claudia <anacnogueira@gmail.com>
2026-02-25 13:57:04 -0300

    Refatorando classe DB

--- src/controllers/livro.controller.php
+++ src/controllers/livro.controller.php
@@ -2,6 +2,10 @@
 
 $id = $_REQUEST['id'];
 
-$livro = (new DB)->livro($id);
+$livro = (new DB)->query(
+    query: "select * from livros WHERE usuario_id = 1 AND  id = :id",
+    class: Livro::class,
+    params: ['id' => $id]  
+)->fetch();
 
 view('livro', compact('livro'));
--- src/database.php
+++ src/database.php
@@ -6,32 +6,20 @@ class DB
 
     public function __construct()
     {
-
         $this->db = new PDO('sqlite:database.sqlite');
     }
 
-    public function livros($pesquisa = null)
+    public function query($query, $class = null, $params = [])
     {
-        /*$sql = "SELECT * FROM livros";
-        if ($pesquisa) {
-            $sql .= " WHERE titulo LIKE '%{$pesquisa}%' OR autor LIKE '%{$pesquisa}%' OR descricao LIKE '%{$pesquisa}%'";
-        }*/
+        $prepare = $this->db->prepare($query);
 
-        $prepare = $this->db->prepare("SELECT * FROM livros WHERE usuario_id = 1 AND titulo LIKE :pesquisa");
-        $prepare->bindValue("pesquisa", "%$pesquisa%");        
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-        $prepare->execute();
+        if ($class) {
+            $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
+        }
 
-        return $prepare->fetchAll();
+        $prepare->execute($params);
+
+        return $prepare;
     }
-
-    public function livro($id)
-    {
-        $prepare = $this->db->prepare("select * from livros WHERE usuario_id = 1 AND  id = :id");
-        $prepare->bindParam("id", $id);
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-        $prepare->execute();
-
-        return $prepare->fetch();
-    }
+ 
 }
