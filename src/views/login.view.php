264fee4b473110c5ed6a5e192512442e9c220e4b
Ana Claudia <anacnogueira@gmail.com>
2026-02-25 15:48:15 -0300

    Registrando um usuário

--- src/views/login.view.php
+++ src/views/login.view.php
@@ -1,7 +1,7 @@
 <div class="mt-6 grid grid-cols-2 gap-2">
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
-        <form action="" class="p-4 space-y-4">
+        <form method="post" class="p-4 space-y-4">
             <div class="flex flex-col"> 
                 <label class="text-stone-400 mb-1">Email</label>
                 <input type="email" name="email" required class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full">
@@ -17,8 +17,10 @@
     </div>
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registro</h1>
-         <form class="p-4 space-y-4">
-
+         <form method="post" action="/registrar" class="p-4 space-y-4">
+            <?php if (strlen($mensagem > 0)): ?>
+                <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border-2"><?= $mensagem ?></div>
+            <?php endif; ?>
             <div class="flex flex-col"> 
                 <label class="text-stone-400 mb-1">Nome</label>
                 <input type="text" name="nome" required class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full">
@@ -36,7 +38,7 @@
 
             <div class="flex flex-col"> 
                 <label class="text-stone-400 mb-1">Senha</label>
-                <input type="password" name="password" required class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full">
+                <input type="password" name="senha" required class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full">
             </div>
 
             <button type="reset" class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border-2 hover:bg-stone-700">Cancelar</button>
