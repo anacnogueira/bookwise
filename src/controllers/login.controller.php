f9dc874389f95f1e32bf3d76e88e42a7fcb25a78
Ana Claudia <anacnogueira@gmail.com>
2026-02-27 10:29:33 -0300

    Salva senha encriptada e verifica senha no login

--- src/controllers/login.controller.php
+++ src/controllers/login.controller.php
@@ -16,21 +16,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     $usuario = $DB->query(
-        query: "select * from usuarios where email=:email AND senha=:senha",
+        query: "select * from usuarios where email=:email",
         class: Usuario::class,
-        params: compact('email', 'senha')    
+        params: compact('email')    
     )->fetch();
 
-    if ($usuario) {
+    if ($usuario) {        
+        if (!password_verify($senha, $usuario->senha)) {
+            flash()->push('validacoes_login', ['Credenciais inválidas!']);
+            header("Location: /login");
+            exit();
+        }
         $_SESSION['auth'] = $usuario;
         flash()->push('mensagem', "Seja bem-vindo " . $usuario->nome . "!");
         header("Location: /");
         exit();
     } else {
-        flash()->push('mensagem_login', 'Credenciais Inválidas!');
+        flash()->push('validacoes_login', ['Credenciais Inválidas!']);
         header("Location: /login");
         exit();
     }
-}    
+}
+
+
 
 view('login');
